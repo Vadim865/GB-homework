0d62f6d5cb93c745d4e2582dd83cdc758642fbf0
vadim865 <v9800000865@gmail.com>
2022-08-22 21:37:46 +0500

    add task8

--- Lesson1_task8/Program.cs
+++ Lesson1_task8/Program.cs
@@ -4,6 +4,9 @@
 
 Console.WriteLine("Введите число n");
     int n = Convert.ToInt32(Console.ReadLine());
-    for (int i=1; n>=1; i++);
-    if (n%2==0) 
-    Console.Write("Число =");
+    
+    for (int i = 2; i < n; i++)
+    
+    if (i%2==0) 
+    
+    {Console.WriteLine(i);}
