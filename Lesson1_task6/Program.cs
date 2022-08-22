0ad9a552fb1078f472815d790e8267dd40315121
vadim865 <v9800000865@gmail.com>
2022-08-22 09:13:12 +0500

    add task

--- Lesson1_task6/Program.cs
+++ Lesson1_task6/Program.cs
@@ -2,3 +2,18 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Bool flagwin = false;
+{
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    int a/2;
+
+    if a/2 == int number;
+    flagwin = true;
+    Console.Write("Да");
+
+    flagwin = false;
+    Console.Write("Нет");
+    }
