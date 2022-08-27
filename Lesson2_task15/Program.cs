b40dba9dd51e756ca6b51a39ea2d25566ccced86
vadim865 <v9800000865@gmail.com>
2022-08-28 01:40:17 +0500

    add task15

--- Lesson2_task15/Program.cs
+++ Lesson2_task15/Program.cs
@@ -4,4 +4,15 @@
 // 7 -> да
 // 1 -> нет
 
+Console.WriteLine("Введите цифры от 1 до 7 обозначающую день недели");
+int n =Convert.ToInt32(Console.ReadLine());
 
+if (n==6 || n==7)
+{
+    Console.WriteLine("да");
+ }
+
+else
+{ 
+    Console.WriteLine("нет");
+    }
