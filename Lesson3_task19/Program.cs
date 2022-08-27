ec39aa729120aba387028077c36a10309fc31b8d
vadim865 <v9800000865@gmail.com>
2022-08-28 02:18:47 +0500

    add task19

--- Lesson3_task19/Program.cs
+++ Lesson3_task19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+string str = Console.ReadLine();
+
+char[]n = str.ToCharArray();
+
+if (n[0]==n[4] || n[1]==n[3])
+{
+    Console.WriteLine("да");
+ }
+
+else
+{ 
+    Console.WriteLine("нет");
+    }
