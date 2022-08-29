35b400ecb0919fedccd5a5d2cac984bef6ff9c3b
vadim865 <v9800000865@gmail.com>
2022-08-30 00:26:42 +0500

    add task29

--- Lesson4_task29/Program.cs
+++ Lesson4_task29/Program.cs
@@ -3,3 +3,18 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+int ReadInt(string m)
+{
+    Console.Write(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
