f8e0eae581b7bbd84563bc45f59d3dbf37ffce86
vadim865 <v9800000865@gmail.com>
2022-09-05 01:42:42 +0500

    add Lesson9_task64

---
+++ Lesson9_task64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+        
+		
+
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[m,n];
+        int nat array = 0
+
+        for (int i = 0; i <n; i++)
+		   if (m > n);
+           (m + 1);
+         Console.Write(m + ", ");
+		
+		
+    }
+}
