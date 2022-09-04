31ee4d1d45f4d93908eacb9775bfe5d94ee28e43
vadim865 <v9800000865@gmail.com>
2022-09-04 19:15:42 +0500

    add task36

--- Lesson5_task36/Program.cs
+++ Lesson5_task36/Program.cs
@@ -4,16 +4,20 @@
 // [-4, -6, 89, 6] -> 0
 
 
-int s = 5;
+int s = 4;
 int [] a = new int [s];
-int chetSum = 0;
+int nechetSum = 0;
 
 Random random = new Random ();
 
 for (int i=0; i<s; i++)
 {
-Console.WriteLine(a[i] = random.Next(100));
-    if (a[i] % 2 == 0)
+Console.WriteLine(a[i] = random.Next(-100,100));
+    if (a[i] % 2 != 0)
 
-chetSum += a[i];
+nechetSum += a[i];
+}
+ 
+{
+ Console.WriteLine("Сумма нечётных чисел в массиве: {0}", nechetSum);
 }
