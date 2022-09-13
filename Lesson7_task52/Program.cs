88102a42f49f4701303a1e78ef2593d8a3daca59
vadim865 <v9800000865@gmail.com>
2022-09-14 01:16:18 +0500

    add Lesson7_task52

--- Lesson7_task52/Program.cs
+++ Lesson7_task52/Program.cs
@@ -5,15 +5,54 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//static Random rnd = new Random();
+//static void Main(string[] args)
+//int[,] array = new int[a, b];
+
+ 
+//for (int i = 0; i < array.GetLength(0); i++)
 
 
+//for (int i=0;i<array;i++){
+  // for (int j=0;j<array;j++) {
+    //   a[i,j]=i*j+1;
+      // Console.Write(a[i,j]+ " ");
+   //}
+   //Console.Write(a[i,j]+ " ");
+//}
+using System.Linq;
+ 
+namespace ConsoleApplication1{
 
-for (int i=0;i<c;i++){
-   for (int j=0;j<c;j++) {
-       a[i,j]=i*j+1;
-       Console.Write(a[i,j]+ " ");
-   }
-   Console.Write(a[i,j]+ " ");
-}
-
-    
+    class Program
+    {
+        const int N = 5;
+        const int M = 6; 
+ 
+        static Random rnd = new Random();
+ 
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[N, M]; 
+ 
+            
+            for (int i = 0; i < N; i++)
+                matrix.SetRow(Enumerable.Range(0, M).Select(x => x = rnd.Next(-10, 20)).ToArray(), i);
+            
+            
+            for (int i = 0; i < N; i++)
+                Console.WriteLine(string.Join("\t", matrix.GetRow(i)));
+ 
+            
+            var result = Enumerable.Range(0, M).Select(x => matrix.GetColumn(x).Sum() / M);
+ 
+            
+            Console.WriteLine();
+            Console.WriteLine(string.Join("\t", result));
+ 
+            Console.ReadKey(true);
+        }
+        }    }
